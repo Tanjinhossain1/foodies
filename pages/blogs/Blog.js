2053e3cba5f0a21f3e5289f6734ecee9249109c7
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-07-28 00:55:43 +0600

    blog update

--- pages/blogs/Blog.js
+++ pages/blogs/Blog.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function index({ food }) {
-  const { name, img } = food;
+  // const { name, img } = food;
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
-        <figure><img src={img} alt="Shoes" /></figure>
+        {/* <figure><img src={img} alt="Shoes" /></figure> */}
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
+          {/* <h2 className="card-title">{name}</h2> */}
           <p>If a dog chews shoes whose shoes does he choose?</p>
 
         </div>
